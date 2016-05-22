4725df1af4f3ed9f575b2dcbfc1e2867bd5c952e
bohara2000 <bryant.ohara@gmail.com>
2016-05-22 13:14:29 -0400

    Update rendersong.sh
    Added note about SIOD error that occurs when running rendersong.sh

--- rendersong.sh
+++ rendersong.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# NOTE - if you get the following error:
+# SIOD ERROR: wrong type of argument to get_c_val
+# then see http://linuxsagas.digitaleagle.net/2013/09/15/synthesizing-an-accapella-song-with-festival-speech-synthesis/
+# for a workaround
+
 counter=0
 for f in `ls output-?.xml`
 do
